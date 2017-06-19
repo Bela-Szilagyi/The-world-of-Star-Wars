1db6676aca5934355eb562a1b4b38a690676addc
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-06-19 08:03:37 +0200

    main page basic layout implemented without working buttons

---
+++ static/js/js.js
@@ -0,0 +1,53 @@
+function main() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'http://swapi.co/api/planets/',
+        success: function(response) {
+            console.log(response.count);
+            console.log(response.next);
+            console.log(response.previous);
+            console.log(response.results);
+            console.log(response.results.length);
+            var planets = response.results
+            for (i = 0; i < planets.length; i++) {
+                console.log(planets[i]);
+                let planet = planets[i];
+                let name = planet.name;
+                let diameter = Number(planet.diameter).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' km';
+                let climate = planet.climate;
+                let terrain = planet.terrain;
+                let surfaceWater = planet.surface_water;
+                if (surfaceWater !== 'unknown') {
+                    surfaceWater = planet.surface_water + ' %';
+                };
+                if (planet.population !== 'unknown') {
+                    var population = Number(planet.population).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' people';
+                } else {
+                    var population = planet.population;
+                };
+                let residents = planet.residents;
+                switch(residents.length) {
+                    case 0: {
+                        residents = 'No known residents';
+                        break;    
+                    }
+                    case 1: {
+                        residents = '<button type="button" class="btn btn-default">1 resident</button>';
+                        break;
+                    }
+                    default: {
+                        residents = '<button type="button" class="btn btn-default">' + residents.length + ' residents</button>'
+                        break;    
+                    }
+                };
+                $('tbody').append('<tr><td>' + name + '</td><td>' + diameter + '</td><td>' + climate + '</td><td>' + terrain + '</td><td>' + surfaceWater + '</td><td>' + population + '</td><td>' + residents + '</td></tr>');
+            }
+        },
+        error: function() {
+            alert('Error loading planets data!');
+        } 
+    });
+}
+
+$(document).ready(main);
