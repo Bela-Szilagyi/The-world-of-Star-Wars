09b753ab38c8fa149e47686d895c4ba01e037b29
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-06-22 18:22:38 +0200

    delete console logs

--- static/js/js.js
+++ static/js/js.js
@@ -4,7 +4,6 @@ function main() {
     // $('h1').append(test);
     
     var userCookie = getUserCookie('username');
-    console.log(userCookie);
     if (userCookie) {
         alert('logged in as ' + userCookie);
     } else {
@@ -16,21 +15,18 @@ function main() {
 
     $('#next').on('click', function() {
         pageNumber++;
-        console.log(pageNumber);
         $('#planets').empty(); 
         handlePlanets(pageNumber, userCookie);
     });
 
     $('#previous').on('click', function() {
         pageNumber--;
-        console.log(pageNumber);
         $('#planets').empty();
         handlePlanets(pageNumber, userCookie); 
     });
 
     $('.test').on('click', function() {
         let data = JSON.stringify({planet:this.id});
-        console.log('!!!!!!!!!!data ' + data); 
         $.ajax({
             type : 'POST',
             url : "/vote/",
@@ -48,7 +44,6 @@ function main() {
     $('#statistics').on('click', function() {
         $.post( '/statistics/', function(response) {
         for (const property in response) {            
-            console.log(property + ' ' + response[property]);
             debugger;
             handleStatistics(property, response[property]);
         }
@@ -80,11 +75,6 @@ function handlePlanets(page, cookie) {
         dataType: 'json',
         url: 'http://swapi.co/api/planets/?page=' + page,
         success: function(response) {
-            console.log(response.count);
-            console.log(response.next);
-            console.log(response.previous);
-            console.log(response.results);
-            console.log(response.results.length);
             handlePageButtons(response.next, response.previous);
             displayPlanets(response.results, cookie);
         },
@@ -98,22 +88,18 @@ function handlePageButtons(next, previous) {
     var $next = $('#next');
     var $previous = $('#previous');
     if ($next.hasClass('disabled') && next !== null) {
-        console.log('enabling next');
         $next.removeClass('disabled');
         $next.removeProp('disabled');
     };
     if (! $next.hasClass('disabled') && next === null) {
-        console.log('disabling next');
         $next.addClass('disabled');
         $next.prop('disabled', 'disabled');
     };
     if ($previous.hasClass('disabled') && previous !== null) {
-        console.log('enabling previous');
         $previous.removeClass('disabled');
         $previous.removeProp('disabled');
     };
     if (! $previous.hasClass('disabled') && previous === null) {
-        console.log('disabling previous');
         $previous.addClass('disabled');
         $previous.prop('disabled', 'disabled');
     };
@@ -122,7 +108,6 @@ function handlePageButtons(next, previous) {
 function displayPlanets(planets, cookie) {
     var residentURLs = {};
     for (i = 0; i < planets.length; i++) {
-        console.log(planets[i]);
         var planet = planets[i];
         var planetId = (planet.url).slice(28, -1);
         var name = planet.name;
@@ -144,7 +129,6 @@ function displayPlanets(planets, cookie) {
         };
         var residentsArray = planet.residents;
         residentURLs[name] = residentsArray;
-        console.log('residentURLs ' + residentURLs);
         switch(residentsArray.length) {
             case 0: {
                 var residentsNumber = 'No known residents';
@@ -161,17 +145,13 @@ function displayPlanets(planets, cookie) {
         };
         if (cookie) {
             let imageTag = '<img src="/static/images/vote.png") class="vote" id="' + planetId + '" title="vote for ' + name + '" alt="vote" width="32" height="32">';
-            console.log(imageTag);
             $('#planets').append('<tr><td>' + name + '</td><td>' + diameter + '</td><td>' + climate + '</td><td>' + terrain + '</td><td>' + surfaceWater + '</td><td>' + population + '</td><td>' + residentsNumber + '</td><td>' + imageTag + '</td></tr>');
         } else {
             $('#planets').append('<tr><td>' + name + '</td><td>' + diameter + '</td><td>' + climate + '</td><td>' + terrain + '</td><td>' + surfaceWater + '</td><td>' + population + '</td><td>' + residentsNumber + '</td></tr>');
         };
     };
     localStorage.setItem(residentURLs, residentURLs);
-    console.log(localStorage);
     $('.resident').on('click', function() {
-        console.log(this.id);
-        console.log(name);
         $('#residents-modal-title').append('Residents of ' + this.id);
         $('.close-modal').on('click', function() {
             $('#residentsModal').modal('hide');
@@ -184,7 +164,6 @@ function displayPlanets(planets, cookie) {
     $('.vote').on('click', function() {
         alert(this.id);
         let vote = JSON.stringify({vote:this.id, username:cookie});
-        console.log('!!!!!!!!!!vote ' + vote); 
         $.ajax({
             type : 'POST',
             url : "/vote/",
@@ -202,7 +181,6 @@ function displayPlanets(planets, cookie) {
 
 function handleResidents(residents) {
     for (let i = 0; i < residents.length; i++) {
-        console.log(residents[i]);
         handleResident(residents[i]);
     }
 }
